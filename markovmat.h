84aaf68ecad7339fb20dee2b58e77a8e0600c329
Sidney Araujo Melo <sidneyaraujomelo@gmail.com>
2016-02-03 08:37:16 -0300

    estruturando melhor

---
+++ markovmat.h
@@ -0,0 +1,21 @@
+#ifndef MARKOVMAT_H_INCLUDED
+#define MARKOVMAT_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+
+float ** newMatriz(int, int);
+
+void zeroMatrix(float **, int, int);
+
+void printMatriz(float **, int, int);
+
+float ** multiplyMatriz(float **, float **, int, int,int);
+
+float ** expMatriz(float **, int, int);
+
+float ** metodoMatriz(float **, float **, int, int);
+
+float metodoChapman(float **, int, int, int, int);
+
+#endif // MARKOVMAT_H_INCLUDED
